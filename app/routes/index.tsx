834daa8e9e7625f262f30b5fefad456c02f36640
wurikiji <wurikiji@gmail.com>
2022-10-21 00:44:38 +0900

    config!: vitest, cf workers
    Revert "feat: edit index.tsx"
    
    This reverts commit 6870184b686214ec01d7dd38dd621e873dd5d392.
    
    env!: change dev and deploy env
    
    - npm to yarn
    - vercel to cloudflare
    - jest to vitest
    
    chore: styled-component validation
    
    chore: remove styled-components info from release
    
    feat: react v18
    
    chore: downgrade typescript
    
    deploy!: change to cloudflare workers

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,7 +11,6 @@ export default function Index() {
   console.log("유저 데이터", datas);
 
   return (
-<<<<<<< HEAD
     <MainWrapper>
       <GridWrapper>
         <TopBanner />
@@ -21,14 +20,6 @@ export default function Index() {
         <MainContent>메인 컨텐츠.</MainContent>
       </GridWrapper>
     </MainWrapper>
-=======
-    <div>
-      <div>
-        <Link to="logIn">log in</Link>
-      </div>
-      <Link to="loginDetail">Detail</Link>
-    </div>
->>>>>>> 0eefa3b (feat login Detail)
   );
 }
 const MainWrapper = styled.div`
