452079bd90bcee56fd209ec30968b66cf9b342fe
Flying-Developer <kopasd99@gmail.com>
2022-10-21 13:27:09 +0900

    Revert "feat login2"
    This reverts commit aa94d412f46822524868706ae4f9703d2ec9b1a3.

--- app/routes/logIn/index.tsx
+++ app/routes/logIn/index.tsx
@@ -1,18 +1,8 @@
 import styled from "styled-components"
-import GoogleLogin from 'react-google-login';
 
-const responseGoogle = (response) => {
-  console.log(response);
-}
 export default function logIn() {
   return (
     <Wrapper>
-      <GoogleLogin
-        clientId="189399095236-k1thmc2f2r2ves3ueb1j2520cuqbdkaj.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-      />,
       <Font>
         <Insert>
           <HW>
