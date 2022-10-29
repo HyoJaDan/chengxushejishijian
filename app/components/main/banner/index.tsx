482a6563069fd356952d8deeaf0980d3869de4ac
wurikiji <wurikiji@gmail.com>
2022-10-30 03:08:10 +0900

    chore: add alt attributes to the image

--- app/components/main/banner/index.tsx
+++ app/components/main/banner/index.tsx
@@ -2,7 +2,11 @@ import type { FC, HTMLProps } from 'react';
 import styled from 'styled-components';
 
 export const Banner: FC<HTMLProps<HTMLElement>> = ({ className }) => (
-  <Wrapper className={className} src='/banner-area.png' />
+  <Wrapper
+    className={className}
+    src='/banner-area.png'
+    alt='메인 페이지 배너 영역'
+  />
 );
 
 const Wrapper = styled.img`
