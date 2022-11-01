33af5cb881411b7d468241ffc19907b31bd76f66
Flying-Developer <kopasd99@gmail.com>
2022-11-01 13:17:12 +0900

    feat loginDetail

--- app/hooks/useScript.tsx
+++ app/hooks/useScript.tsx
@@ -2,13 +2,7 @@ import { useEffect } from "react";
 
 export const useScript = (url: string, onload: any) => {
   useEffect(() => {
-    console.log("url", url);
-    console.log("onload", onload);
     let script = document.createElement("script");
-<<<<<<< HEAD
-=======
-
->>>>>>> 1f94979 (Done GoogleLogIn)
     script.src = url;
     script.onload = onload;
 
