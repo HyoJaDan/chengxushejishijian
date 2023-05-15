82aad4771276acaadb366c2b2544fbb6fccb1f6f
Flying-Developer <kopasd99@gmail.com>
2023-05-15 14:35:19 +0900

    build : accepts remix V2

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createRoutesFromFolders } = require('@remix-run/v1-route-convention');
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget: 'vercel',
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
   server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
-  // serverBuildPath: "api/index.js",
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "functions/[[path]].js",
-  // publicPath: "/build/",
+  future: {
+    // makes the warning go away in v1.15
+    v2_routeConvention: true,
+  },
+
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
+  },
 };
