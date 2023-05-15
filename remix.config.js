c2f42ee775f820d203a2114428829d68d45048a6
Flying-Developer <kopasd99@gmail.com>
2023-05-15 14:54:45 +0900

    build : accepts remix V2-3

--- remix.config.js
+++ remix.config.js
@@ -1,18 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { createRoutesFromFolders } = require('@remix-run/v1-route-convention');
-
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  // Tell Remix to ignore everything in the routes directory.
-  // We'll let `createRoutesFromFolders` take care of that.
-  ignoredRouteFiles: ['**/*'],
-  routes: (defineRoutes) => {
-    // `createRoutesFromFolders` will create routes for all files in the
-    // routes directory using the same default conventions as Remix v1.
-    return createRoutesFromFolders(defineRoutes, {
-      // If you're already using `ignoredRouteFiles` in your Remix config,
-      // you can move them to `ignoredFilePatterns` in the plugin's options.
-      ignoredFilePatterns: ['**/.*', '**/*.css'],
-    });
-  },
+  serverBuildTarget: 'vercel',
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  // serverBuildPath: "api/index.js",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "functions/[[path]].js",
+  // publicPath: "/build/",
 };
