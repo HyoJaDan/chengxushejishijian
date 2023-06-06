569ebbac57a79d0fd41fca5c8ed0b55b7d40dd72
Flying-Developer <kopasd99@gmail.com>
2023-06-06 20:02:49 +0900

    feat : add new banner

--- app/components/main/banner.tsx
+++ app/components/main/banner.tsx
@@ -1,21 +1,48 @@
 import type { FC, HTMLProps } from 'react';
 import styled from 'styled-components';
 
-export const Banner: FC<HTMLProps<HTMLElement>> = ({ className }) => (
-  <Wrapper>
-    <Img
-      className={className}
-      src='/icons/banner-area.png'
-      alt='메인 페이지 배너 영역'
-    />
-  </Wrapper>
-);
+export const Banner: FC<HTMLProps<HTMLElement>> = ({ className }) => {
+  return (
+    <Overlay>
+      <Wrapper>
+        <div className='title2_BD'>평소 공부하면서 모르는 것이 많았다고요?</div>
+        <div className='title2_BD'>당신이 필요한 장소는 바로 이곳입니다!</div>
+        <div style={{ marginTop: '15px' }} className='body2_BD'>
+          모르는 것을 질문하고 대답하여
+        </div>
+        <div className='body2_BD'>다른 사용자와 함께 성장하세요!</div>
+      </Wrapper>
+    </Overlay>
+  );
+};
 
-const Wrapper = styled.div`
+const Overlay = styled.div`
   max-width: 1256px;
   margin: 40px auto;
 `;
-const Img = styled.img`
-  width: 100%;
-  position: relative;
+const Wrapper = styled.div`
+  max-width: 1256px;
+  margin: 40px auto;
+  height: 272px;
+  background-color: ${(prop) => prop.theme.color.grayScale.gray_300};
+  border: 1px solid #efedea;
+  border-radius: 8px;
+  padding: 52px 72px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const Category = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 6px 8px;
+  gap: 4px;
+  width: fit-content;
+  height: 35px;
+  background: white;
+  border-radius: 4px;
+  color: black;
 `;
--- app/components/main/index.tsx
+++ app/components/main/index.tsx
@@ -1,17 +1,11 @@
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
-import {
-  ProblemCategoryId,
-  getProblems,
-  numOfProblems,
-} from '~/data/problem/get-problemList';
+import { ProblemCategoryId, getProblems } from '~/data/problem/get-problemList';
 import { TrainBox } from '../problem/problem-box';
 
 export const Training = () => {
   const sortBy = useRecoilValue(ProblemCategoryId);
   const problems = useRecoilValue(getProblems(sortBy));
-  const numOfProblem = useSetRecoilState(numOfProblems);
-  numOfProblem(problems.length);
   const problem = problems.map((data, index) => {
     return TrainBox(data, index, 1256);
   });
